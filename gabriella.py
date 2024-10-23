7190d48c85d1d5b5882483558f7383b049171b5b
lukyamuzi gabriella <g.bayinze004@gmail.com>
2024-10-23 18:04:25 +0300

    Create gabriella.py

---
+++ gabriella.py
@@ -0,0 +1,8 @@
+#A little about myself
+names = "Gabriella Angel Bayinze Lukyamuzi."
+dob = "10/01/2004.";
+gender = "Female";
+nationality = "Ugandan.";
+hobbies = "Swimming,Travelling, Shopping, Watching movies, Family time,Friends.";
+
+print (f"My name is {names} I was born on {dob} I am a {gender} {nationality} I love {hobbies}");
